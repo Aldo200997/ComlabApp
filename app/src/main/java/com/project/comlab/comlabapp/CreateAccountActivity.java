24118a3fa61504c0bcbd127754886b7012aebc6f
Aldair <omaraldair200997@gmail.com>
2017-04-17 18:25:38 -0500

    Acabamos de crear la interfaz de Crear cuenta

--- app/src/main/java/com/project/comlab/comlabapp/CreateAccountActivity.java
+++ app/src/main/java/com/project/comlab/comlabapp/CreateAccountActivity.java
@@ -2,12 +2,26 @@ package com.project.comlab.comlabapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class CreateAccountActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        String titleToolbar = getResources().getString(R.string.create_account_toolbar_str);
+        showToolbar(titleToolbar, true);
+    }
+
+
+
+    private void showToolbar(String title, boolean backButton){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(backButton);
     }
 }
--- app/src/main/java/com/project/comlab/comlabapp/MainActivity.java
+++ app/src/main/java/com/project/comlab/comlabapp/MainActivity.java
@@ -1,13 +1,27 @@
 package com.project.comlab.comlabapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_register = (Button) findViewById(R.id.login_button_register);
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CreateAccountActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
