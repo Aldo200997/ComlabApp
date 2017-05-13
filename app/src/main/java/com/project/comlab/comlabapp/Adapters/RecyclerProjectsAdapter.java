e063bf261b485f89661fb921fdcc1a3295419e95
Aldair <omaraldair200997@gmail.com>
2017-05-13 16:12:25 -0500

    La app ya consume los proyectos desde un rv en el fragment correspondiente

---
+++ app/src/main/java/com/project/comlab/comlabapp/Adapters/RecyclerProjectsAdapter.java
@@ -0,0 +1,81 @@
+package com.project.comlab.comlabapp.Adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.codesgood.views.JustifiedTextView;
+import com.project.comlab.comlabapp.POJO.ProjectsModel;
+import com.project.comlab.comlabapp.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * Created by aldodev20 on 13/05/17.
+ */
+
+public class RecyclerProjectsAdapter extends RecyclerView.Adapter<RecyclerProjectsAdapter.ViewHolder>{
+
+    List<ProjectsModel> projectList;
+    Context context;
+
+    int[] colors = {R.color.black, R.color.purple, R.color.indigo, R.color.blue, R.color.cyan,
+            R.color.green, R.color.yellow, R.color.orange, R.color.brown};
+
+    // Numero random del 0 a 9
+    int randomNum = (int)(Math.floor(Math.random() * 10 ));
+
+    public RecyclerProjectsAdapter(Context context, List<ProjectsModel> projectList){
+        this.context = context;
+        this.projectList = projectList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_projects, parent, false);
+
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.title.setText(projectList.get(position).getTitle());
+        holder.description.setText(projectList.get(position).getDescription());
+        holder.owner.setText(projectList.get(position).getOwner());
+        holder.image.setBackgroundColor(context.getResources().getColor(colors[(position + randomNum) % 9]));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return projectList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        CircleImageView user_image;
+        TextView owner;
+        ImageView image;
+        TextView title;
+        JustifiedTextView description;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            user_image = (CircleImageView) itemView.findViewById(R.id.item_project_user_photo);
+            owner = (TextView) itemView.findViewById(R.id.item_project_owner);
+            image = (ImageView) itemView.findViewById(R.id.item_project_image);
+            title = (TextView) itemView.findViewById(R.id.item_project_title);
+            description = (JustifiedTextView) itemView.findViewById(R.id.item_project_description);
+        }
+    }
+}
--- app/src/main/java/com/project/comlab/comlabapp/Fragments/ProjectsFragment.java
+++ app/src/main/java/com/project/comlab/comlabapp/Fragments/ProjectsFragment.java
@@ -3,16 +3,34 @@ package com.project.comlab.comlabapp.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.project.comlab.comlabapp.Adapters.RecyclerProjectsAdapter;
+import com.project.comlab.comlabapp.POJO.ProjectsModel;
 import com.project.comlab.comlabapp.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProjectsFragment extends Fragment {
+
+    RecyclerView rv_projects;
+    RecyclerProjectsAdapter adapter;
+    private FirebaseDatabase database;
+    private DatabaseReference reference;
+    List<ProjectsModel> projectList;
 
 
     public ProjectsFragment() {
@@ -23,6 +41,35 @@ public class ProjectsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_projects, container, false);
+
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference("projects");
+
+        rv_projects = (RecyclerView) view.findViewById(R.id.rv_projects);
+        rv_projects.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+
+        projectList = new ArrayList<>();
+        adapter = new RecyclerProjectsAdapter(getContext(), projectList);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                projectList.removeAll(projectList);
+                for (DataSnapshot snap: dataSnapshot.getChildren()) {
+                    ProjectsModel project = snap.getValue(ProjectsModel.class);
+                    projectList.add(project);
+                }
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
         return view;
     }
 
